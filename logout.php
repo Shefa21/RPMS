2cf1f6de2532b2943221969074ea8178f5bfce51
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2025-03-26 20:03:38 +0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$_SESSION = array();
+session_destroy();
+
+if (ini_get("session.use_cookies")) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+
+header("Location: login_form.php");
+exit();
+?>
